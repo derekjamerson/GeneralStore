16f5e18879cbae85768a50db34180792f304a706
Derek Jamerson <derekjamerson@gmail.com>
2021-11-03 08:48:43 -0400

    Models done

---
+++ GeneralStore.Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralStore.Models
+{
+    class Product
+    {
+        [Key]
+        public int SKU { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Cost { get; set; }
+        [Required]
+        public int NumberInInventory { get; set; }
+        public bool IsInStock => NumberInInventory > 0;
+    }
+}
