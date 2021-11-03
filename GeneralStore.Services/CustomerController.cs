67d01276e1eba900cf65d3a4566051524ade422c
Derek Jamerson <derekjamerson@gmail.com>
2021-11-03 15:26:47 -0400

    Finished Customer controller

--- GeneralStore.Services/CustomerController.cs
+++ GeneralStore.Services/CustomerController.cs
@@ -2,6 +2,7 @@
 using GeneralStore.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,7 @@ namespace GeneralStore.Services
         {
             if (model is null)
                 return BadRequest("Request body cannot be empty.");
+
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
@@ -27,12 +29,57 @@ namespace GeneralStore.Services
 
         }
         //Get All Customers(GET)
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAllCustomers()
+        {
+            List<Customer> customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
 
         //Get a Customer by its ID(GET)
+        [HttpGet]
+        public async Task<IHttpActionResult> GetCustomerById([FromUri] int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+                return Ok(customer);
+
+            return NotFound();
+        }
 
         //Update an existing Customer by its ID(PUT)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateCustomer([FromUri] int id, [FromBody]Customer updatedModel)
+        {
+            if (id != updatedModel.Id)
+                return BadRequest("IDs do not match.");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            Customer customer = await _context.Customers.FindAsync(id);
+
+            if (customer is null)
+                return NotFound();
+
+            customer.FirstName = updatedModel.FirstName;
+            customer.LastName = updatedModel.LastName;
+            await _context.SaveChangesAsync();
+            return Ok($"\"{customer.FullName}\" successfully updated.");
+        }
 
         //Delete an existing Customer by its ID(DELETE)
+        [HttpDelete]
+        public async Task<IHttpActionResult> RemoveCustomer([FromUri] int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+
+            if (customer is null)
+                return NotFound();
+
+            _context.Customers.Remove(customer);
+            return Ok($"\"{customer.FullName}\" successfully removed.");
+        }
 
     }
 }
